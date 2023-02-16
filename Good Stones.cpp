96a7b05fc32a60db70f1ffc722e64ea14f16da7b
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-02-16 06:20:40 +0530

    Create Good Stones.cpp

---
+++ Good Stones.cpp
@@ -0,0 +1,65 @@
+class Solution{
+
+public:
+
+    int goodStones(int n,vector<int> &arr){
+
+        vector<bool>reached(n,false);
+
+        int ans=0,c_c=0;
+
+        set<int>s;
+
+        vector<int>tmp;
+
+        for(int i=0;i<n;i++){
+
+            if(reached[i])
+
+                continue;
+
+            int j=i;
+
+            while(!reached[j]){
+
+                reached[j]=true;
+
+                tmp.push_back(j);
+
+                j=j+arr[j];
+
+                if(j>=n || j<0){
+
+                    for(auto k:tmp){
+
+                        s.insert(k);
+
+                    }
+
+                    tmp.clear();
+
+                    break;
+
+                }
+
+            }
+
+            if(tmp.size() && s.count(j)){
+
+                for(auto k:tmp){
+
+                    s.insert(k);
+
+                }
+
+            }
+
+            tmp.clear();
+
+        }
+
+        return s.size();
+
+    }  
+
+};
