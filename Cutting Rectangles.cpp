d4b5b2b3984fa08f4890474f86383b7ba65f38da
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-03-03 12:39:45 +0530

    Create Cutting Rectangles.cpp

---
+++ Cutting Rectangles.cpp
@@ -0,0 +1,10 @@
+class Solution{
+public:
+vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long S = __gcd(L, B); // inbuilt function in C++
+        vector<long long> res = {(L*B)/(S*S), S};
+        return res;
+    }
+
+};
