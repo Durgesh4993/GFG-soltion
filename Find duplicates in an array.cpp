f7366d60adfc24394db7f9f4c066d35326924d8a
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-03-14 10:56:50 +0530

    Create Find duplicates in an array.cpp

---
+++ Find duplicates in an array.cpp
@@ -0,0 +1,22 @@
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        bool flag=0;
+        for(int i=0; i<n; i++) {
+            mp[arr[i]]++;
+        }
+        
+        for(auto x : mp) {
+            if(x.second > 1) {
+                ans.push_back(x.first);
+                flag=1;
+                
+            }
+        }
+        sort(ans.begin(), ans.end());
+        if(flag==1) return ans;
+        else return {-1};
+    }
+};
