0bac620051574270e1cfe61b7d8e0f0b3c01929e
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-04-07 10:25:49 +0530

    Create Add Minimum Characters.cpp

---
+++ Add Minimum Characters.cpp
@@ -0,0 +1,35 @@
+class Solution{   
+public:
+ int addMinChar(string str){    
+
+        string s;
+        int N=str.length();
+        int low=0,high=N-1,count=0;
+        
+        while(low<high){
+            if(str[high]==str[low])
+            {
+                count++;
+                low++;
+                high--;
+            }
+           else
+            {
+                low=0;
+                count=0;
+                if(str[0]!=str[high])
+                {
+                    high--;
+                }
+            }
+        }
+        
+       // cout<<count<<" ";
+        if(low==high)
+            count=count*2+1;
+        else
+         count=count*2;
+        
+        return   N-count;
+    }
+};
