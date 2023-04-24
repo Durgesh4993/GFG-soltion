6fc63dba6bee1e9e60c0a8f92a3a3a2494c44266
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-04-24 18:27:21 +0530

    Update A difference of values and indexes.cpp

--- A difference of values and indexes.cpp
+++ A difference of values and indexes.cpp
@@ -4,7 +4,6 @@ int maxDistance(int arr[], int n)
         int a = INT_MIN, b = INT_MAX, c = INT_MIN, d = INT_MAX;
         for(int i=0; i<n; i++)
         {
-
             a = max(a, arr[i]-i);
 
             b = min(b, arr[i]-i);
