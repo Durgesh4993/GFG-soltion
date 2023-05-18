1c14911748ba425f6f286d9b18d92df35143cb5a
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-05-18 17:41:07 +0530

    Update Absolute List Sorting.cpp

--- Absolute List Sorting.cpp
+++ Absolute List Sorting.cpp
@@ -2,13 +2,6 @@ class Solution{
 public:
     Node* sortList(Node* head)
     {
-        //As the numbers are already sorted we observe if while traversing
-        // a -ve number comes it will be lowest till now and should go in the
-        // beginning of the list so we do just that by changing the links
-        // to get a better feel how this can be done please draw on paper and
-        // try to change the links to get in order
-        // the idea i have used is to keep a previous pointer ,curr and we 
-        //can use the same variable head
         Node *curr = head->next,*prev = head;  
         while(curr)
         
