d4315d9d47837c14e2a60910040a315942d0b1d0
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-06-11 18:03:24 +0530

    Create Adding Ones.cpp

---
+++ Adding Ones.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i=0;i<k;i++){
+            a[updates[i]-1]++;
+        }
+        for(int i=1;i<n;i++){
+            a[i]+=a[i-1];
+        }
+    }
+};
