1978d5df6b34bcfa790378fa34f426f4ecc5f211
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-07-11 17:48:28 +0530

    Update Find duplicates in an array.cpp

--- Find duplicates in an array.cpp
+++ Find duplicates in an array.cpp
@@ -7,7 +7,8 @@ class Solution{
         for(int i=0; i<n; i++) {
             mp[arr[i]]++;
         }
-        
+
+      
         for(auto x : mp) {
             if(x.second > 1) {
                 ans.push_back(x.first);
