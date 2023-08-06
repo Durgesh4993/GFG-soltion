f0a234e54499e2e3c247c25a010592b9983eaa48
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-08-06 06:49:57 +0530

    Update A Game of LCM.cpp

--- A Game of LCM.cpp
+++ A Game of LCM.cpp
@@ -6,6 +6,7 @@ class Solution {
       
         // code here
         long long int ans = N * (N-1);
+      
         int c=0;
         for(long long int i=N-2; i>2 && c<2; i--){          
             if(__gcd(ans,i)==1){           
