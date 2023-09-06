0166d77beffc02593f6a40da04586ce0c83f9622
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-09-06 19:49:56 +0530

    Update A Game of LCM.cpp

--- A Game of LCM.cpp
+++ A Game of LCM.cpp
@@ -1,7 +1,6 @@
 class Solution {
   public:
-    long long maxGcd(int N) {   
-      
+    long long maxGcd(int N) {      
         // code here
         long long int ans = N * (N-1);
         int c=0;
